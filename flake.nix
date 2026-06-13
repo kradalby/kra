ea6afe27871350b4e2af1650a4ea21161382159b
Kristoffer Dalby <kristoffer@dalby.cc>
2026-06-13 20:43:23 +0200

    ci: run checks via kradalby/flake-checks

--- flake.nix
+++ flake.nix
@@ -4,192 +4,225 @@
   inputs = {
     nixpkgs.url = "nixpkgs/nixpkgs-unstable";
     utils.url = "github:numtide/flake-utils";
+    flake-checks.url = "github:kradalby/flake-checks";
+    flake-checks.inputs.nixpkgs.follows = "nixpkgs";
   };
 
-  outputs = {
-    self,
-    nixpkgs,
-    utils,
-    ...
-  }: let
-    kraVersion =
-      if (self ? shortRev)
-      then self.shortRev
-      else "dev";
-  in
+  outputs =
+    { self
+    , nixpkgs
+    , utils
+    , flake-checks
+    , ...
+    }:
+    let
+      kraVersion =
+        if (self ? shortRev)
+        then self.shortRev
+        else "dev";
+    in
     {
-      overlays.default = _: prev: let
-        pkgs = nixpkgs.legacyPackages.${prev.stdenv.hostPlatform.system};
-      in {
-        krapage = pkgs.callPackage ({buildGoModule}:
-          buildGoModule.override { go = pkgs.go_1_26; } {
-            pname = "krapage";
-            version = kraVersion;
-            src = pkgs.nix-gitignore.gitignoreSource [] ./.;
+      overlays.default = _: prev:
+        let
+          pkgs = nixpkgs.legacyPackages.${prev.stdenv.hostPlatform.system};
+        in
+        {
+          krapage = pkgs.callPackage
+            ({ buildGoModule }:
+              buildGoModule.override { go = pkgs.go_1_26; } {
+                pname = "krapage";
+                version = kraVersion;
+                src = pkgs.nix-gitignore.gitignoreSource [ ] ./.;
 
-            subPackages = ["cmd/krapage"];
+                subPackages = [ "cmd/krapage" ];
 
-            vendorHash = "sha256-ChkPjkBCTagY2d+JQybDm3Lw3cTLURktWvolxe9xT+w=";
-          }) {};
-      };
+                vendorHash = "sha256-ChkPjkBCTagY2d+JQybDm3Lw3cTLURktWvolxe9xT+w=";
+              })
+            { };
+        };
     }
     // utils.lib.eachDefaultSystem
-    (system: let
-      pkgs = import nixpkgs {
-        overlays = [self.overlays.default];
-        inherit system;
-      };
-      buildDeps = with pkgs; [
-        git
-        gnumake
-        go_1_26
-      ];
-      devDeps = with pkgs;
-        buildDeps
-        ++ [
-          golangci-lint
-          entr
+      (system:
+      let
+        pkgs = import nixpkgs {
+          overlays = [ self.overlays.default ];
+          inherit system;
+        };
+        fc = flake-checks.lib;
+        common = {
+          inherit pkgs;
+          root = ./.;
+          pname = "krapage";
+          version = kraVersion;
+          vendorHash = "sha256-ChkPjkBCTagY2d+JQybDm3Lw3cTLURktWvolxe9xT+w=";
+          goPkg = pkgs.go_1_26;
+          embedDirs = [ ./data ];
+        };
+        buildDeps = with pkgs; [
+          git
+          gnumake
+          go_1_26
         ];
-    in {
-      # `nix develop`
-      devShells.default = pkgs.mkShell {
-        buildInputs = with pkgs;
-          [
-            (writeShellScriptBin
-              "krarun"
-              ''
-                go run ./cmd/krapage --verbose
-              '')
-            (writeShellScriptBin
-              "kradev"
-              ''
-                fd .go | entr -r krarun
-              '')
+        devDeps = with pkgs;
+          buildDeps
+          ++ [
+            golangci-lint
+            entr
+          ];
+      in
+      {
+        # `nix develop`
+        devShells.default = pkgs.mkShell {
+          buildInputs = with pkgs;
+            [
+              (writeShellScriptBin
+                "krarun"
+                ''
+                  go run ./cmd/krapage --verbose
+                '')
+              (writeShellScriptBin
+                "kradev"
+                ''
+                  fd .go | entr -r krarun
+                '')
 
-            (writeShellScriptBin
-              "nix-vendor-sri"
-              ''
-                set -eu
-                OUT=$(mktemp -d -t nar-hash-XXXXXX)
-                rm -rf "$OUT"
-                go mod vendor -o "$OUT"
-                go run tailscale.com/cmd/nardump --sri "$OUT"
-                rm -rf "$OUT"
-              '')
+              (writeShellScriptBin
+                "nix-vendor-sri"
+                ''
+                  set -eu
+                  OUT=$(mktemp -d -t nar-hash-XXXXXX)
+                  rm -rf "$OUT"
+                  go mod vendor -o "$OUT"
+                  go run tailscale.com/cmd/nardump --sri "$OUT"
+                  rm -rf "$OUT"
+                '')
 
-            (writeShellScriptBin
-              "go-mod-update-all"
-              ''
-                cat go.mod | ${pkgs.silver-searcher}/bin/ag "\t" | ${pkgs.silver-searcher}/bin/ag -v indirect | ${pkgs.gawk}/bin/awk '{print $1}' | ${pkgs.findutils}/bin/xargs go get -u
-                go mod tidy
-              '')
-          ]
-          ++ devDeps;
-      };
-
-      # `nix build`
-      packages = with pkgs; {
-        inherit krapage;
-        default = krapage;
-      };
-
-      # `nix run`
-      apps = {
-        krapage = utils.lib.mkApp {
-          drv = pkgs.krapage;
+              (writeShellScriptBin
+                "go-mod-update-all"
+                ''
+                  cat go.mod | ${pkgs.silver-searcher}/bin/ag "\t" | ${pkgs.silver-searcher}/bin/ag -v indirect | ${pkgs.gawk}/bin/awk '{print $1}' | ${pkgs.findutils}/bin/xargs go get -u
+                  go mod tidy
+                '')
+            ]
+            ++ devDeps;
         };
-        default = utils.lib.mkApp {
-          drv = pkgs.krapage;
+
+        # `nix build`
+        packages = with pkgs; {
+          inherit krapage;
+          default = fc.goBuild common;
         };
-      };
-    })
+
+        formatter = fc.formatter common;
+
+        checks = {
+          build = fc.goBuild common;
+          gotest = fc.goTest common;
+          golangci-lint = fc.goLint common;
+          formatting = fc.goFormat common;
+        };
+
+        # `nix run`
+        apps = {
+          krapage = utils.lib.mkApp {
+            drv = pkgs.krapage;
+          };
+          default = utils.lib.mkApp {
+            drv = pkgs.krapage;
+          };
+        };
+      })
     // {
-      nixosModules.default = {
-        pkgs,
-        lib,
-        config,
-        ...
-      }: let
-        cfg = config.services.krapage;
-      in {
-        options = with lib; {
-          services.krapage = {
-            enable = mkEnableOption "Enable krapage";
+      nixosModules.default =
+        { pkgs
+        , lib
+        , config
+        , ...
+        }:
+        let
+          cfg = config.services.krapage;
+        in
+        {
+          options = with lib; {
+            services.krapage = {
+              enable = mkEnableOption "Enable krapage";
 
-            package = mkOption {
-              type = types.package;
-              description = ''
-                krapage package to use
-              '';
-              default = pkgs.krapage;
+              package = mkOption {
+                type = types.package;
+                description = ''
+                  krapage package to use
+                '';
+                default = pkgs.krapage;
+              };
+
+              user = mkOption {
+                type = types.str;
+                default = "krapage";
+                description = "User account under which krapage runs.";
+              };
+
+              group = mkOption {
+                type = types.str;
+                default = "krapage";
+                description = "Group account under which krapage runs.";
+              };
+
+              dataDir = mkOption {
+                type = types.path;
+                default = "/var/lib/krapage";
+                description = "Path to data dir";
+              };
+
+              tailscaleKeyPath = mkOption {
+                type = types.path;
+              };
+
+              verbose = mkOption {
+                type = types.bool;
+                default = false;
+              };
+
+              localhostPort = mkOption {
+                type = types.port;
+                default = 56661;
+              };
+
+              environmentFile = mkOption {
+                type = types.nullOr types.path;
+                default = null;
+                example = "/var/lib/secrets/krapageSecrets";
+              };
             };
-
-            user = mkOption {
-              type = types.str;
-              default = "krapage";
-              description = "User account under which krapage runs.";
-            };
-
-            group = mkOption {
-              type = types.str;
-              default = "krapage";
-              description = "Group account under which krapage runs.";
-            };
-
-            dataDir = mkOption {
-              type = types.path;
-              default = "/var/lib/krapage";
-              description = "Path to data dir";
-            };
-
-            tailscaleKeyPath = mkOption {
-              type = types.path;
-            };
-
-            verbose = mkOption {
-              type = types.bool;
-              default = false;
-            };
-
-            localhostPort = mkOption {
-              type = types.port;
-              default = 56661;
-            };
-
-            environmentFile = mkOption {
-              type = types.nullOr types.path;
-              default = null;
-              example = "/var/lib/secrets/krapageSecrets";
+          };
+          config = lib.mkIf cfg.enable {
+            systemd.services.krapage = {
+              enable = true;
+              script =
+                let
+                  args =
+                    [
+                      "--ts-key-path ${cfg.tailscaleKeyPath}"
+                      "--listen-addr localhost:${toString cfg.localhostPort}"
+                    ]
+                    ++ lib.optionals cfg.verbose [ "--verbose" ];
+                in
+                ''
+                  ${cfg.package}/bin/krapage ${lib.concatStringsSep " " args}
+                '';
+              wantedBy = [ "multi-user.target" ];
+              after = [ "network-online.target" ];
+              wants = [ "network-online.target" ];
+              serviceConfig = {
+                User = cfg.user;
+                Group = cfg.group;
+                Restart = "always";
+                RestartSec = "15";
+                WorkingDirectory = cfg.dataDir;
+                EnvironmentFile = lib.optional (cfg.environmentFile != null) cfg.environmentFile;
+              };
+              path = [ cfg.package ];
             };
           };
         };
-        config = lib.mkIf cfg.enable {
-          systemd.services.krapage = {
-            enable = true;
-            script = let
-              args =
-                [
-                  "--ts-key-path ${cfg.tailscaleKeyPath}"
-                  "--listen-addr localhost:${toString cfg.localhostPort}"
-                ]
-                ++ lib.optionals cfg.verbose ["--verbose"];
-            in ''
-              ${cfg.package}/bin/krapage ${lib.concatStringsSep " " args}
-            '';
-            wantedBy = ["multi-user.target"];
-            after = ["network-online.target"];
-            wants = ["network-online.target"];
-            serviceConfig = {
-              User = cfg.user;
-              Group = cfg.group;
-              Restart = "always";
-              RestartSec = "15";
-              WorkingDirectory = cfg.dataDir;
-              EnvironmentFile = lib.optional (cfg.environmentFile != null) cfg.environmentFile;
-            };
-            path = [cfg.package];
-          };
-        };
-      };
     };
 }
